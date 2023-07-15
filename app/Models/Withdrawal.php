1032a7fa415d3b9c96ad5a55f9a92b2fa3dbe73c
dohd <alloicebrian@gmail.com>
2023-07-15 14:05:31 +0300

    Add deposit and withdrawal model

---
+++ app/Models/Withdrawal.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Withdrawal extends Model
+{
+    protected $table = 'withdrawals';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [];
+
+    /**
+     * The attributes that should be hidden for serialization.
+     *
+     * @var array<int, string>
+     */
+    protected $hidden = [];
+
+    /**
+     * The attributes that should be cast.
+     *
+     * @var array<string, string>
+     */
+    protected $casts = [];
+
+    /**
+     * Dates
+     * @var array
+     */
+    protected $dates = [
+        'created_at',
+        'updated_at'
+    ];
+
+    /**
+     * Guarded fields of model
+     * @var array
+     */
+    protected $guarded = [
+        'id'
+    ];
+
+
+    /**
+     * model life cycle event listeners
+     * @return void
+     */
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::addGlobalScope('owner_id', function ($builder) {
+            $builder->where('owner_id', auth()->user()->owner_id);
+        });
+    }
+}
